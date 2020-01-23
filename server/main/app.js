53b8bd90250ef35e8f01238c70bd93ab5978d2a6
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-23 08:17:23 -0800

    remve node modules

--- server/main/app.js
+++ server/main/app.js
@@ -1,7 +1,10 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+var indexRouter = require('./routes')
 
 var app = express();
 
@@ -11,5 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use('/', indexRouter)
 
 module.exports = app;
