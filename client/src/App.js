6e81111d06ae72624c1bd2052be0bbbec691f84a
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-23 08:57:22 -0800

    addsql schema

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
 
 
 const App = () => {
+/**
+ * I can use useEffect here to get the currently logged in user in order to give the current
+ * user to the whole app.
+ */
+    
+  
+   const [currentUser, setUser] = useState('Nobody')
 
     return(
       <div>
       React IS Working
+    <p>{currentUser}</p>
       </div>
     )
 }
 
 
 export default App;
+
+/**
+ * 
+ */
