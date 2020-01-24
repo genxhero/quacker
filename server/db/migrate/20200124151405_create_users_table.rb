8774d57636da06ff2c2ab1b7dd65aa2f60173f1f
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-24 09:23:47 -0800

    get all this stuff added

---
+++ server/db/migrate/20200124151405_create_users_table.rb
@@ -0,0 +1,19 @@
+class CreateUsersTable < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :description, null: false, default: ""
+      t.string :first_name, null: false, default: "Anonymous"
+      t.string :last_name, null: false, default: ""
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.integer :followings, array: true
+      t.integer :followers, array: true
+      t.timestamps
+    end
+    add_index :users, :username
+    add_index :users, :email
+    add_index :users, :session_token
+  end
+end
