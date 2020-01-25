98a34dc4b495d8080aa240b8e5b2f8a4506b3fb9
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-25 12:55:10 -0800

    write up first draft of quacks table migration

--- server/db/migrate/20200125205233_create_quacks.rb
+++ server/db/migrate/20200125205233_create_quacks.rb
@@ -1,6 +1,11 @@
 class CreateQuacks < ActiveRecord::Migration[6.0]
   def change
     create_table :quacks do |t|
+      t.integer :user_id, null: false
+      t.integer :quack_id
+      t.integer :body
+      t.integer :requackers, array: true, default: []
+      t.timestamps
     end
   end
 end
