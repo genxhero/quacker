b9fb432b9b519128c5dae2fe1ae436c6e7416116
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-25 11:48:39 -0800

    refactor quacks to sometimes belong to another quack

--- server/app/models/post.rb
+++
@@ -1,8 +0,0 @@
-class Post < ApplicationRecord
-
-    belongs_to :user,
-    primary_key: :id,
-    foreign_key: :user_id,
-    class_name: "User"
-
-end
--- server/app/models/quack.rb
+++ server/app/models/quack.rb
@@ -1,3 +1,13 @@
-class Quack < Post
+class Quack < ApplicationRecord\
+
+    belongs_to :user,
+    primary_key: :id, 
+    foreign_key: :user_id,
+    class_name: "User"
+
+    belongs_to :quack, optional: true
+    primary_key: :id,
+    foreign_key: :quack_id,
+    class_name: "Quack"
 
 end
