d190571cc8daab3509df3dbd27fe5c3dc106f9e9
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-27 08:47:22 -0800

    first query field

--- server/app/graphql/types/query_type.rb
+++ server/app/graphql/types/query_type.rb
@@ -4,10 +4,9 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :all_quacks, [QuackType], null: false
+    def images
+     Quack.all.order(created_at: :desc)
     end
   end
 end
