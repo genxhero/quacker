42923087d8b1a99e9b76fc2dbba3357e55bcc1e3
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-27 07:32:18 -0800

    build sample requackresolver

--- server/mutations/add_requack.rb
+++ server/mutations/add_requack.rb
@@ -1,4 +1,15 @@
 module Mutations
     class AddRequack < BaseMutation
+        argument :user_id, Integer, required: true
+        argument :quack_id, Integer, required: true
+        argument :comment, String, required: false
+
+        def resolve(user_id, quack_id, comment: nil)
+            quack = Quack.new
+            quack.user_id = user_id
+            quack.quack_id = quack_id 
+            quack.comment = comment
+        end
+
     end
 end
