17cb7074f0acf7bba7fc82087ce8a38318f4b379
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-28 07:36:28 -0800

    add search toggle

--- client/src/components/QuackForm.jsx
+++ client/src/components/QuackForm.jsx
@@ -2,12 +2,12 @@ import React, {useState, useEffect} from 'react';
 
 const QuackForm = props => {
     const handleChange = (e) => {
-        debugger;
-
         if (e.nativeEvent.data === "@") {
             e.target.style.color = "blue"
+            toggleSearch(true)
         } else if (e.nativeEvent.data === " ") {
             e.target.style.color = "black"
+            toggleSearch(false)
         }
         updateQuack(e.target.value)
     }
@@ -18,6 +18,7 @@ const QuackForm = props => {
     }
 
     const [quack, updateQuack] = useState("")
+    const [searching, toggleSearch] = useState(false)
     return (
         <div>
             <textarea value={quack} onChange={handleChange}>
