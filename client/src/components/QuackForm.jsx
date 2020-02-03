2482928e94857f9057f07dd90b08f735f4ba1367
Aaron Goddard <genxhero@users.noreply.github.com>
2020-02-03 13:56:14 -0800

    revert changes that were breaking the code

--- client/src/components/QuackForm.jsx
+++ client/src/components/QuackForm.jsx
@@ -41,51 +41,16 @@ const QuackForm = props => {
       );
 
     const [userSearch, updateSearch ] = useState({query: "", searching: false});
-        console.log("Am Searching?", userSearch.searching, "For:", userSearch.query)
 
+  
     return (
         <div>
             <p>{<span>Current User: {props.currentUser}</span>}</p>
             <Editor editorState={editorState} onChange={setEditorState}>
             </Editor>
-            {(userSearch.searching && userSearch.query.length > 2 ) && <UserSearch userSearch={userSearch} />}
+            {(userSearch.searching && userSearch.query.length > 1 ) && <UserSearch userSearch={userSearch} />}
         </div>
     )
 }
 
 export default QuackForm;
-
-/**
- *     const handleChange = (e) => {
-        if (e.nativeEvent.data === "@") {
-            e.target.style.color = "blue"
-            toggleSearch(true)
-        } else if (e.nativeEvent.data === " ") {
-            e.target.style.color = "black"
-            updateQuery("")
-            toggleSearch(false)
-        }
-        if (searching && e.nativeEvent.data !== "@" && e.nativeEvent.data !== " ") {
-             updateQuery(`${query}${e.nativeEvent.data}`)
-        }
-        updateQuack(e.target.value)
-    }
-
-    const autoSearch = (e) => {
-        console.log("searching...")
-        
-    }
-
-
-    const [quack, updateQuack] = useState("")
-    const [searching, toggleSearch] = useState(false)
-    const [query, updateQuery] = useState("")
-
-        <div>
-                { searching && <span>Searching...for {query}</span>}
-            </div>
-            <div>
-    {          <span>Your Quack is: {quack}</span>}
-            </div>
-
- */
