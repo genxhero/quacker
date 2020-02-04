b47e80de9cc5f49010eaf2a25b98c1c079f56f80
Aaron Goddard <genxhero@users.noreply.github.com>
2020-02-04 11:04:33 -0800

    dangerously set inner html wrapped in div hopefully it works

--- client/src/components/QuackForm.jsx
+++ client/src/components/QuackForm.jsx
@@ -71,7 +71,7 @@ const QuackForm = props => {
             </Editor>
             <button style={{"padding":"2rem", "color":"black"}}onClick={saveQuack} value="Quack">Quack</button>
             {userSearch.searching  && <UserSearch userSearch={userSearch} />}
-            {quack && {dangerouslySetInnerHTML={__html: quack} } }
+            {quack && <div dangerouslySetInnerHTML={`__html: ${quack}`}> </div> }
         </div>
     )
 }
