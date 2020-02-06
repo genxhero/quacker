81415eee316f7a656252f66401fec394509f266c
Aaron Goddard <genxhero@users.noreply.github.com>
2020-02-06 08:04:54 -0800

    start the homefeed component

---
+++ client/src/components/HomeFeed.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Quack from './Quack';
+import { useQuery } from '@apollo/react-hooks';
+
+const HomeFeed = (props) => {
+    return (
+        <div className="quack-feed">
+
+        </div>
+    )
+}
