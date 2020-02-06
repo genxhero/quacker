c3ae882f263aed5cd57f33d26136e8f334292d3d
Aaron Goddard <genxhero@users.noreply.github.com>
2020-02-06 08:17:11 -0800

    import quackernav

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import QuackForm from './components/QuackForm';
+import HomeFeed from './components/HomeFeed';
+import QuackerNav from './components/QuackerNav';
 import { ApolloProvider } from 'react-apollo';
 import client from './utils/apollo';
-import {Router, Switch} from 'react-router-dom';
+import {Router, Switch, Route} from 'react-router-dom';
 const App = () => {
 /**
  * I can use useEffect here to get the currently logged in user in order to give the current
@@ -14,21 +15,27 @@ const App = () => {
     return(
       <ApolloProvider client={client}>
          <Router>
-        <div className="quacker-app">
-          <div className="quacker-nav">
-            <ul>
-              <li>home</li>
-              <li>explore</li>
-              <li>notifications</li>
-              <li>messages</li>
-              <li>bookmarks</li>
-              <li>lists</li>
-              <li>profile</li>
-            </ul>
-          </div>
+          <div className="quacker-app">
+            
+            <div className="quacker-nav">
+              <ul>
+                <li>home</li>
+                <li>explore</li>
+                <li>notifications</li>
+                <li>messages</li>
+                <li>bookmarks</li>
+                <li>lists</li>
+                <li>profile</li>
+              </ul>
+            </div>
+
+            <Switch>
+              <Route exact path = "/home" component={HomeFeed} />
+            </Switch>
+         
 
           <div>
-          
+            All the stuff on the right goes here.
           </div>
         </div>
         </Router>
