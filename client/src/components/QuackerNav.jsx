cd285de3815f7d69f467f5474e2efe287e3d556d
Aaron Goddard <genxhero@users.noreply.github.com>
2020-02-10 08:44:30 -0800

    rearrange links

--- client/src/components/QuackerNav.jsx
+++ client/src/components/QuackerNav.jsx
@@ -3,11 +3,10 @@ import {Link} from 'react-router-dom';
 
 const QuackerNav = (props) => {
     return (
-    <div className="">
-        
+    <div style={{"display":"flex", "flexDirection":"column"}}>
           <Link to="/home">home</Link>
           <Link to="/explore">explore</Link>
-          <li>notifications</li>
+          <Link to="/">notifications</Link>
           <li>messages</li>
           <li>bookmarks</li>
           <li>lists</li>
