2ca351d0cc42be8fd6fe419e793b2bd7555bfbcc
Aaron Goddard <genxhero@users.noreply.github.com>
2020-02-10 10:20:20 -0800

    wrap link content in div

--- client/src/components/QuackerNav.jsx
+++ client/src/components/QuackerNav.jsx
@@ -4,9 +4,21 @@ import {Link} from 'react-router-dom';
 const QuackerNav = (props) => {
     return (
     <div style={{"display":"flex", "flexDirection":"column"}}>
-          <Link to="/home">home</Link>
-          <Link to="/explore">explore</Link>
-          <Link to="/">notifications</Link>
+          <Link to="/home">
+             <div className="nav-link-content">
+                 <span>Home</span>
+             </div>
+          </Link>
+          <Link to="/explore">
+             <div className="nav-link-content">
+                 <span>Explore</span>
+             </div>
+          </Link>
+          <Link to="/">
+              <div className="nav-link-content">
+                  <span>Notifications</span>
+              </div>
+          </Link>
           <li>messages</li>
           <li>bookmarks</li>
           <li>lists</li>
