c8748c5ff1a8c473688f038bcc81b6ae0a9282db
Aaron Goddard <genxhero@users.noreply.github.com>
2020-02-16 15:08:56 -0800

    handle loading and errors for quackfeed

--- client/src/components/HomeFeed.jsx
+++ client/src/components/HomeFeed.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Quack from './Quack';
 import QuackForm from './QuackForm';
 import { useQuery } from '@apollo/react-hooks';
+import homeFeedQuacks from '../queries/homeFeedQuacks';
 
 /**
  * This component renders 
@@ -9,9 +10,18 @@ import { useQuery } from '@apollo/react-hooks';
 
 const HomeFeed = (props) => {
     const [quacking, toggleQuackForm] = useState(false);
+    const {loading, error, data} = useQuery(homeFeedQuacks, {
+        variables: { id: 1},
+      });
+    if (loading) return <p id="quackbody"> Loading ...</p>;
+    if (error) return <p id="quackbody">Error...</p>
+    debugger;
     return (
         <div className="quack-feed">
             You quack me up
+            <div className="col-lg">
+                {}
+            </div>
             <div className="new-quack-btn"onClick={() => toggleQuackForm(true)}> Quack</div>
             {quacking && <QuackForm close={() => toggleQuackForm(false)} />}
         </div>
