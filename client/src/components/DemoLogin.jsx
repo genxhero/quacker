699c96adaab55a3d96e5291f9faeca5ccacbb734
Aaron Goddard <genxhero@users.noreply.github.com>
2020-10-02 10:12:11 -0700

    something mismatched about graphql queyr

--- client/src/components/DemoLogin.jsx
+++ client/src/components/DemoLogin.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import {useMutation} from '@apollo/react-hooks';
+import LOGIN_USER from '../mutations/loginUser';
 
 const DemoLogin = () => {
-    return <div className="login-btn">
-        <span>Demo Login</span>
+    const [loginUser, { data }] = useMutation(LOGIN_USER);
+    const login = (e) => {
+        e.preventDefault();
+        loginUser({variables: {input: {email: "fiery@swagger.com", password: "demodemo"}}, refetchQueries: ['homeFeedQuacks']}).then(res => {
+            debugger;
+        })
+    }
+
+    return <div className="login-btn-container">
+        <button className="login-btn"onClick={login} value="login">Demo Login</button>
     </div>
 }
 
