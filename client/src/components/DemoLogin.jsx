b8dfabba849f8e7e413fdd090a87e022889ff8b8
Aaron Goddard <genxhero@users.noreply.github.com>
2020-10-06 09:36:52 -0700

    blah

--- client/src/components/DemoLogin.jsx
+++ client/src/components/DemoLogin.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useMutation} from '@apollo/react-hooks';
 import LOGIN_USER from '../mutations/loginUser';
 
 const DemoLogin = () => {
     const [loginUser, { data }] = useMutation(LOGIN_USER);
+
+
+
     const login = (e) => {
         e.preventDefault();
-        loginUser({variables: {email: "fiery@swagger.com", password: "demodemo"}}).then(res => {
+        loginUser({variables: {email: "fiery@swagger.com", password: "demodemo"}, refetchQueries: ["currentUser"]}).then(res => {
+            const user = res.data.loginUser.user;
             debugger;
         })
     }
