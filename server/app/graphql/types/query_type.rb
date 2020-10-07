8e6304c76922a170f616d4241db71ae27df50caa
Aaron Goddard <genxhero@users.noreply.github.com>
2020-10-07 11:03:53 -0700

    remove useless boilerplate comments

--- server/app/graphql/types/query_type.rb
+++ server/app/graphql/types/query_type.rb
@@ -1,9 +1,6 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
+    
     field :all_quacks, [QuackType], null: false
     def all_quacks
      Quack.all.order(created_at: :desc)
